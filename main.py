10a316e801112e49f88c426256627d316ec4fe5f
PtspluS <pierre.marez@isen.yncrea.fr>
2021-05-21 08:06:05 +0200

    Add all the template for cards type + remove template_event.html

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pdfkit
 import pandas as pd
 import json
-import sys
+import os
 from django.template import Template, Context
 
-cards_csv = pd.read_csv("./cards.csv")
+cards = pd.read_csv("./cards.csv", sep=";")
 
 with open("./config.json") as js:
     config = json.load(js)
+
+qd_cards = sum(cards["quantity"])
+print("Quantity of cards = %i" % qd_cards)
+print("Number of sheets generated = %i" % (qd_cards % 9 + qd_cards // 9))
+try:
+    print("Creating directory %s" % config["name"])
+    os.mkdir("./" + config["name"])
+except:
+    exit("Error ! Impossible to create directory")
+
+
+
 
 print(0)
